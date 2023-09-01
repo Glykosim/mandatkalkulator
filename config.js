92eef86365ebbfbaf47c451a3291bd80e996e79d
Glykosim <142170909+Glykosim@users.noreply.github.com>
2023-09-01 19:41:00 +0200

    Lots of changes.

--- config.js
+++ config.js
@@ -1,15 +1,17 @@
+const majorityMandates = 18; // Endre dette tallet til hva som gir flertall
+
 const parties = [
-  {name: 'H', mandates: 9, color: '#0065f1', secondaryColor: '#560078'},
-  {name: 'Frp', mandates: 7, color: '#09367F', secondaryColor: '#FFFFFF'},
-  {name: 'AP', mandates: 5, color: '#E4002B', secondaryColor: '#ffd4d9'},
-  {name: 'SV', mandates: 4, color: '#539760', secondaryColor: '#EB4040'},
-  {name: 'AL', mandates: 3, color: '#217D92', secondaryColor: '#ACC90B'},
-  {name: 'INP', mandates: 3, color: '#F54C01', secondaryColor: '#00ABC7'},
+  {name: 'H', mandates: 9, color: '#0065f1', txtColor: '#FFFFFF'},
+  {name: 'Frp', mandates: 7, color: '#09367F', txtColor: '#FFFFFF'},
+  {name: 'AP', mandates: 5, color: '#E4002B', txtColor: '#FFFFFF'},
+  {name: 'SV', mandates: 4, color: '#EB4040', secondaryColor: '#539760', txtColor: '#FFFFFF'},
+  {name: 'AL', mandates: 3, color: '#217D92', secondaryColor: '#ACC90B', txtColor: '#FFFFFF'},
+  {name: 'INP', mandates: 3, color: '#F54C01', secondaryColor: '#00ABC7', txtColor: '#FFFFFF'},
   {name: 'MDG', mandates: 1, color: '#4D7E00', secondaryColor: '#F9F7E6'},
-  {name: 'Krf', mandates: 1, color: '#EE403A', secondaryColor: '#FFD773'},
-  {name: 'V', mandates: 1, color: '#006666', secondaryColor: '#51d694'},
-  {name: 'SP', mandates: 1, color: '#00843d', secondaryColor: '#7DBC23'},
+  {name: 'Krf', mandates: 1, color: '#FFD773', secondaryColor: '#EE403A', txtColor: '#FFFFFF'},
+  {name: 'V', mandates: 1, color: '#006666', secondaryColor: '#51d694', txtColor: '#FFFFFF'},
+  {name: 'SP', mandates: 1, color: '#7DBC23', secondaryColor: '#00843d', txtColor: '#FFFFFF'},
     // ... legg til flere partier etter behov
 ];
 
-const majorityMandates = 18; // Endre dette tallet til hva som gir flertall
+
--- scripts.js
+++ scripts.js
@@ -7,12 +7,17 @@ parties.forEach(party => {
   let btn = document.createElement("button");
   btn.innerHTML = `${party.name} (${party.mandates})`;
   
+  if (party.txtColor) {
+    btn.style.color = party.txtColor;
+  }
+  
     if (party.secondaryColor) {
     btn.style.background = `linear-gradient(90deg, ${party.color} 0%, ${party.color} 40%, ${party.secondaryColor} 60%, ${party.secondaryColor} 100%)`;
   } else {
     btn.style.backgroundColor = party.color;
   }
-  
+
+updateMandateBar();  
 
   btn.onclick = function() {
     if (selectedParties.includes(party)) {
@@ -29,17 +34,58 @@ parties.forEach(party => {
   document.getElementById("partyButtons").appendChild(btn);
 });
 
+function launchConfetti() {
+  for (let i = 0; i < 100; i++) { 
+    let confetti = document.createElement("div");
+    confetti.classList.add("confetti");
 
-const buttonHeight = 45; // Høyden av en knapp som definert i CSS
-const buttonMargin = 10; // Margin definert i CSS (topp og bunn)
-const totalButtonHeight = parties.length * (buttonHeight + 2*buttonMargin); // Total høyde = antall knapper * (høyde + 2*margin)
-document.getElementById('mandateBar').style.height = `${totalButtonHeight}px`;
+    confetti.style.left = `50vw`;
+    confetti.style.top = `50vh`;
+
+    const rotation = Math.random() * 360;  // Initialiser rotasjonen først
+    confetti.dataset.rotation = rotation;
+    confetti.style.setProperty('--rotation', `${rotation}deg`);
+
+    const distance = 100;  
+    const radianRotation = rotation * Math.PI / 180;  // Konverterer rotasjonen til radianer
+    const xOffset = distance * Math.cos(radianRotation);
+    const yOffset = distance * Math.sin(radianRotation);
+    confetti.style.setProperty('--xOffset', `${xOffset}vw`);
+    confetti.style.setProperty('--yOffset', `${yOffset}vh`);
+
+    confetti.style.backgroundColor = getRandomColor();
+    document.body.appendChild(confetti);
+
+    setTimeout(() => {
+        confetti.remove();
+    }, 2000);
+  }
+}
+
+
+
+
+// const buttonHeight = 45; // Høyden av en knapp som definert i CSS
+// const buttonMargin = 10; // Margin definert i CSS (topp og bunn)
+// const totalButtonHeight = parties.length * (buttonHeight + 2*buttonMargin); // Total høyde = antall knapper * (høyde + 2*margin)
+// document.getElementById('mandateBar').style.height = `${totalButtonHeight}px`;
+
+function getRandomColor() {
+  // Hent alle farger fra de valgte partiene
+  const colors = selectedParties.flatMap(party => {
+    if(party.secondaryColor) {
+      return [party.color, party.secondaryColor];
+    }
+    return [party.color];
+  });
+
+  return colors[Math.floor(Math.random() * colors.length)];
+}
 
 
 function updateMandateBar() {
   const totalMandates = parties.reduce((acc, party) => acc + party.mandates, 0);
   const selectedTotalMandates = selectedParties.reduce((acc, party) => acc + party.mandates, 0);
-
   const mandateBar = document.getElementById("mandateBar");
 
   // Renser eksisterende innhold
@@ -63,18 +109,29 @@ function updateMandateBar() {
   flertallMarker.className = "flertallMarker";
   
   // Ny beregning for flertallsmarkørens posisjon
-  let flertallPercentage = (majorityMandates / totalMandates) * 100;
-  flertallMarker.style.bottom = `${flertallPercentage}%`;
-  
-  mandateBar.appendChild(flertallMarker);
+ let flertallPercentage = (majorityMandates / totalMandates) * 100;
+    flertallMarker.style.bottom = `${flertallPercentage}%`;
+
+    mandateBar.appendChild(flertallMarker);
+
+    // Legg til tekstetikett for flertallsmarkeringen
+    let flertallLabel = document.createElement("div");
+    flertallLabel.innerHTML = `Flertall ved ${majorityMandates} mandat`;
+    flertallLabel.style.position = "absolute";
+    flertallLabel.style.bottom = `${flertallPercentage - 3}%`;  // litt over flertallsmarkeringen
+    flertallLabel.style.left = "105%";  // litt til høyre for søylen
+    flertallLabel.style.fontSize = "0.8rem";
+
+    mandateBar.appendChild(flertallLabel);
 
   const status = document.getElementById("majorityStatus");
   if (selectedTotalMandates >= majorityMandates) {
     status.textContent = `Sammensetningen av partiene "${selectedParties.map(p => p.name).join('+')}" gir flertall!`;
+	launchConfetti();
   } else {
     const diff = majorityMandates - selectedTotalMandates;
     if (selectedParties.length === 1) {
-      status.textContent = `Partiet ${selectedParties[0].name} kan ikke styre alene, det mangler ${diff} mandat for flertall.`;
+      status.textContent = `${selectedParties[0].name} kan ikke styre alene, det mangler ${diff} mandat for flertall.`;
     } else {
       status.textContent = `Partiene ${selectedParties.map(p => p.name).join('+')} kan ikke styre alene, de mangler ${diff} mandater for flertall.`;
     }
