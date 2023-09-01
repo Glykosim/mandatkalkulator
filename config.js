a2a40ca380e6e4d812ada5552a10256ea3a87c8a
Glykosim <142170909+Glykosim@users.noreply.github.com>
2023-09-01 23:43:28 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const majorityMandates = 18; // Endre dette tallet til hva som gir flertall
 
 const parties = [
-  {name: 'H', mandates: 9, color: '#0065f1', txtColor: '#FFFFFF'},
-  {name: 'Frp', mandates: 7, color: '#09367F', txtColor: '#FFFFFF'},
+  {name: 'H', mandates: 10, color: '#0065f1', txtColor: '#FFFFFF'},
+  {name: 'Frp', mandates: 6, color: '#09367F', txtColor: '#FFFFFF'},
   {name: 'AP', mandates: 5, color: '#E4002B', txtColor: '#FFFFFF'},
-  {name: 'SV', mandates: 4, color: '#EB4040', secondaryColor: '#539760', txtColor: '#FFFFFF'},
+  {name: 'SV', mandates: 3, color: '#EB4040', secondaryColor: '#539760', txtColor: '#FFFFFF'},
   {name: 'AL', mandates: 3, color: '#217D92', secondaryColor: '#ACC90B', txtColor: '#FFFFFF'},
-  {name: 'INP', mandates: 3, color: '#F54C01', secondaryColor: '#00ABC7', txtColor: '#FFFFFF'},
+  {name: 'INP', mandates: 4, color: '#F54C01', secondaryColor: '#00ABC7', txtColor: '#FFFFFF'},
   {name: 'MDG', mandates: 1, color: '#4D7E00', secondaryColor: '#F9F7E6'},
   {name: 'Krf', mandates: 1, color: '#FFD773', secondaryColor: '#EE403A', txtColor: '#FFFFFF'},
   {name: 'V', mandates: 1, color: '#006666', secondaryColor: '#51d694', txtColor: '#FFFFFF'},
-  {name: 'SP', mandates: 1, color: '#7DBC23', secondaryColor: '#00843d', txtColor: '#FFFFFF'},
+  {name: 'PP', mandates: 1, color: '#7ca5ff', secondaryColor: '#0a0772', txtColor: '#FFFFFF'},
     // ... legg til flere partier etter behov
 ];
 
